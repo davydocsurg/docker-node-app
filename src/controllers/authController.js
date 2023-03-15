13346b8f617a080e35f0f8f78a0fae96cec0de37
davydocsurg <davydocsurg@gmail.com>
2023-03-15 14:30:03 +0100

    add register and login controller methods, update package.json

---
+++ src/controllers/authController.js
@@ -0,0 +1,41 @@
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.create({ email, password });
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: user,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res
+                .status(401)
+                .json({ success: false, message: "Invalid email or password" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res
+                .status(401)
+                .json({ success: false, message: "Invalid email or password" });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.json({ success: true, token });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export { register, login };
